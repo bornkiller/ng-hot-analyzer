9bae2b6b654fce173770affba851bb47ba44688a
bornkiller <hjj491229492@hotmail.com>
2016-11-18 00:12:41 +0800

    test: 更新测试文件。

---
+++ test/analyze.spec.js
@@ -0,0 +1,21 @@
+/**
+ * @description - analyze unit suit
+ * @author - bornkiller <hjj491229492@hotmail.com>
+ */
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+
+const { analyzeAccessToken, analyzeInstanceReference, linkAnalyzeStream } = require('../src/analyze');
+const shareModuleTemplate = fs.readFileSync(path.resolve(__dirname, 'fixture', 'share.module.js'), {encoding: 'utf8'});
+
+describe('ng-hot analyze implement', function () {
+  it('analyze access token', function () {
+    let componentInstanceRef  = analyzeInstanceReference(shareModuleTemplate);
+    let componentAccessToken = analyzeAccessToken(shareModuleTemplate);
+    
+    let result = linkAnalyzeStream(componentInstanceRef, componentAccessToken);
+    console.log(result);
+  });
+});
