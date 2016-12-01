ba00faa5434beeec8c81de890f3633ee33e793e7
黄健 <jian.huang03@ele.me>
2016-12-01 14:43:48 +0800

    refactor: 调整token分析策略。

--- src/token.js
+++ src/token.js
@@ -3,13 +3,20 @@
  * @author - bornkiller <hjj491229492@hotmail.com>
  */
 
-// 匹配过滤器导出与模块地址, 匹配工厂函数导出与模块地址, 匹配控制器导出与模块地址
-const standardComponentCapture = [
-  {category: 'factory', reg: /\.factory\((['"])([a-zA-Z]+)\1\,\s*([a-zA-Z]+)\)/gm},
-  {category: 'service', reg: /\.service\((['"])([a-zA-Z]+)\1\,\s*([a-zA-Z]+)\)/gm},
-  {category: 'filter', reg: /\.filter\((['"])([a-zA-Z]+)\1\,\s*([a-zA-Z]+)\)/gm},
-  {category: 'controller', reg: /\.controller\((['"])([a-zA-Z]+)\1\,\s*([a-zA-Z]+)\)/gm},
-  {category: 'directive', reg: /\.directive\((['"])([a-zA-Z]+)\1\,\s*([a-zA-Z]+)\)/gm}
+/**
+ * @description
+ * - 匹配过滤器导出与模块地址, 匹配工厂函数导出与模块地址, 匹配控制器导出与模块地址
+ * - `ng-hmr` 暂时不支持注册`controller`的热更新,暂时搁置,后续`ng-hot-loader`无需修改,只需同步`ng-hmr`跟`ng-hot-analyzer`即可
+ * @type {*[]}
+ */
+const captureRegList = [
+  {category: 'Filter', reg: /\.filter\((['"])([\w]+)\1\,\s*([\w]+)\)/gm},
+  {category: 'Factory', reg: /\.factory\((['"])([\w]+)\1\,\s*([\w]+)\)/gm},
+  {category: 'Service', reg: /\.service\((['"])([a-zA-Z]+)\1\,\s*([a-zA-Z]+)\)/gm},
+  {category: 'Directive', reg: /\.directive\((['"])([\w]+)\1\,\s*([\w]+)\)/gm},
+  {category: 'RouteTemplate', reg: /template:\s*(\w+)/gm},
+  {category: 'RouteController', reg: /controller:\s*(\w+)/gm}
+  // {category: 'controller', reg: /\.controller\((['"])([a-zA-Z]+)\1\,\s*([a-zA-Z]+)\)/gm}
 ];
 
 /* eslint-disable no-cond-assign */
@@ -31,13 +38,15 @@ function analyzeAccessToken(template) {
   let middleware;
   let architecture = [];
 
-  standardComponentCapture.forEach(({category, reg}) => {
+  captureRegList.forEach(({category, reg}) => {
+    let isRouteMode = category === 'RouteTemplate' || category === 'RouteController';
+
     // destruct import ways
     while (middleware = reg.exec(template)) {
       architecture.push({
         category: category,
-        token: middleware[2],
-        name: middleware[3]
+        token: isRouteMode ? 'RouteMark' : middleware[2],
+        name: isRouteMode ? middleware[1] : middleware[3]
       });
     }
   });
